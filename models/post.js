7321dbba4610c8c4816c2c01fc9cadc6bdbe1680
08Uzer <121750611+08Uzair@users.noreply.github.com>
2025-05-11 14:20:57 +0530

    first commit

---
+++ models/post.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: [String],
+    default: [],
+  },
+  isBookMark: {
+    type: Boolean,
+    default: false,
+  },
+  comments: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const post = mongoose.model("post", postSchema);
